42e64c351f91c7f306633655dbd58f1fc672692b
Holden Ahn <holden.ahn@daumkakao.com>
2015-12-03 14:58:52 +0900

    getNearestDayOfWeek()가 항상 과거 날짜만 리턴하는 버그 수정

--- src/main/java/org/jfree/date/SerialDate.java
+++ src/main/java/org/jfree/date/SerialDate.java
@@ -714,14 +714,11 @@ public abstract class SerialDate implements Comparable,
         }
 
         // find the date...
-        final int baseDOW = base.getDayOfWeek();
-        int adjust = -Math.abs(targetDOW - baseDOW);
-        if (adjust >= 4) {
-            adjust = 7 - adjust;
-        }
-        if (adjust <= -4) {
-            adjust = 7 + adjust;
-        }
+        int delta = targetDOW - base.getDayOfWeek();
+        int positiveDelta = delta + 7;
+        int adjust = positiveDelta % 7;
+        if (adjust > 3)
+            adjust -= 7;
         return SerialDate.addDays(adjust, base);
 
     }
