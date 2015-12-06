cdb5ee6d4f25ea140095b5d226602e182302cdd5
HongJoon Ahn <anvatar@gmail.com>
2015-12-07 00:17:17 +0900

    불필요한 index 필드 제거

--- src/main/java/org/jfree/date/WeekdayRange.java
+++ src/main/java/org/jfree/date/WeekdayRange.java
@@ -1,10 +1,5 @@
 package org.jfree.date;
 
 public enum WeekdayRange {
-    LAST(-1), NEAREST(0), NEXT(1);
-    public final int index;
-
-    WeekdayRange(int index) {
-        this.index = index;
-    }
+    LAST, NEAREST, NEXT;
 }
